4dbea96e5697a9a5def0865e45f6ea185b9da86b
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-12 08:37:22 -0400

    update index with ormconfig

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,25 @@ import { buildSchema } from "type-graphql";
 
 
 (async () => {
-    const connection = createConnection()
+    const connection = createConnection({
+        "type": "postgres",
+        "host": "localhost",
+        "port": 5432,
+        "username": "postgres",
+        "database": "postgres",
+        "synchronize": true,
+        "entities": ["./src/models/**/*.ts",
+            "./src/models/*.ts",
+            "./src/**/models/*.ts",
+            "./**/models/*.ts",
+            "./**/models/**/*.ts"
+        ]
+    })
     const schema = await buildSchema({
         resolvers: [
             BookResolver
-        ]
+        ],
+        
     })
 
     const server = new ApolloServer({
