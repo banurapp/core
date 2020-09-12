66830e9d5674514dfc9d5941cd15fec9e214cc15
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-12 08:37:10 -0400

    user resolver

--- src/Users/resolvers/index.ts
+++ src/Users/resolvers/index.ts
@@ -1,5 +1,6 @@
 import { Resolver, Subscription, Mutation, Arg, Query } from 'type-graphql';
 import { User } from '../models/User';
+import { MutateUserInput } from '../inputs/MutateUserInput';
 @Resolver()
 export class UserResolver {
 
@@ -11,5 +12,16 @@ export class UserResolver {
     @Query(() => [User])
     async getUsers() {
         return await User.find()
+    }
+
+    @Mutation(() => User)
+    async updateUser(@Arg("id") id: string, @Arg("data") data: MutateUserInput) {
+        const user = await User.findOneOrFail({ where: { id } })
+
+        Object.assign(user, data)
+
+        await user.save()
+
+        return user
     }
 }
